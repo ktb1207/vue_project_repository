cd0401b390d76a4920fe9181696051fe0b9b8d0f
kongtb <13103820893@163.com>
2020-12-29 23:22:22 +0800

    update vue3 1229 开始

--- vue3-study-project/src/main.js
+++ vue3-study-project/src/main.js
@@ -3,6 +3,8 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import "./styles/reset.css";
+import "./styles/main.scss";
 
 createApp(App)
   .use(store)
--- vue3-study-project/src/views/Home.vue
+++ vue3-study-project/src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="home router-page">
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <h3>vue3.0学习笔记</h3>
+    <h5>一、Vue3.0亮点概括：</h5>
+    <ul>
+      <li>
+        <h6>Performance: 性能比Vue 2.x快1.2~2倍</h6>
+        <p><i>Vue3.0是如何变快的???</i></p>
+        <ol>
+          <li>
+            <h6>diff方法优化:Vue 3.0，新增了静态标记（PatchFlag）</h6>
+            <p></p>
+          </li>
+        </ol>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    // HelloWorld
   }
 };
 </script>
