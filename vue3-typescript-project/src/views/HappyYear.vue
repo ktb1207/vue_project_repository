b44d47d192382c538cb48282cd91acc20063728f
kongtb <13103820893@163.com>
2021-02-18 22:59:35 +0800

    vue3 happyYear初步实现

--- vue3-typescript-project/src/views/HappyYear.vue
+++ vue3-typescript-project/src/views/HappyYear.vue
@@ -5,26 +5,67 @@
     </div>
     <div class="column-wrp">
       <div class="left">
-        <div class="column-text">牛</div>
-        <div class="column-text">气</div>
-        <div class="column-text">冲</div>
-        <div class="column-text">天</div>
-        <div class="column-text">步</div>
-        <div class="column-text">步</div>
-        <div class="column-text">高</div>
+        <div class="column-text" v-for="(name, index) in reactiveLeftTextArr" :key="index">{{ name }}</div>
       </div>
       <div class="right">
-        <div class="column-text">人</div>
-        <div class="column-text">和</div>
-        <div class="column-text">家</div>
-        <div class="column-text">顺</div>
-        <div class="column-text">年</div>
-        <div class="column-text">年</div>
-        <div class="column-text">好</div>
+        <div class="column-text" v-for="(name, index) in reactiveRightTextArr" :key="index">{{ name }}</div>
       </div>
     </div>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent, onMounted, onUpdated, reactive, nextTick, watch } from 'vue';
+export default defineComponent({
+  name: 'HappyYear',
+  setup() {
+    const leftTextArr = ['牛', '气', '冲', '天', '步', '步', '高'];
+    const rightTextArr = ['人', '和', '家', '顺', '年', '年', '好'];
+    const reactiveLeftTextArr = reactive<Array<string>>([]);
+    const reactiveRightTextArr = reactive<Array<string>>([]);
+    let leftStep = 0;
+    let rightStep = 0;
+    const showRightText = () => {
+      if (rightStep > rightTextArr.length - 1) return;
+      const clearRightTimeOut = setTimeout(() => {
+        reactiveRightTextArr.push(rightTextArr[rightStep]);
+        rightStep += 1;
+        if (rightStep >= leftTextArr.length - 1) clearTimeout(clearRightTimeOut);
+      }, 600);
+    };
+
+    const showLeftText = () => {
+      if (leftStep > leftTextArr.length - 1) {
+        showRightText();
+        return;
+      }
+      const clearLeftTimeOut = setTimeout(() => {
+        reactiveLeftTextArr.push(leftTextArr[leftStep]);
+        leftStep += 1;
+        if (leftStep >= leftTextArr.length - 1) clearTimeout(clearLeftTimeOut);
+      }, 600);
+    };
+
+    watch(reactiveLeftTextArr, () => {
+      nextTick(() => {
+        showLeftText();
+      });
+    });
+    watch(reactiveRightTextArr, () => {
+      nextTick(() => {
+        showRightText();
+      });
+    });
+    onMounted(() => {
+      showLeftText();
+    });
+    onUpdated(() => {
+      console.log('更新了');
+    });
+    return { reactiveLeftTextArr, reactiveRightTextArr };
+  }
+});
+</script>
 
 <style lang="scss">
 .happy-year {
@@ -35,12 +76,16 @@
   .row-title {
     padding: 16px 0;
     flex: 0 0 auto;
+    text-align: center;
+    font-size: 48px;
   }
   .column-wrp {
     flex: 1 1 auto;
     display: flex;
     flex-direction: row;
     justify-content: space-between;
+    font-size: 42px;
+    position: relative;
   }
 }
 </style>
