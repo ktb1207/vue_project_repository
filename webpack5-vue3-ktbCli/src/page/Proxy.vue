a0cc7722519871bdf2fdfe7762a8284b52ffdb8e
kongtb <13103820893@163.com>
2021-11-18 20:59:39 +0800

    update webpack5-vue3-ktbCli npm change to yarn

--- webpack5-vue3-ktbCli/src/page/Proxy.vue
+++ webpack5-vue3-ktbCli/src/page/Proxy.vue
@@ -17,35 +17,48 @@
       <li>proxy代理对象可以作为vue响应方法reactive的参数</li>
       <li>修改reactive代理对象，proxy代理对象handle可以捕获</li>
     </ul>
+    <hr />
   </div>
 </template>
 
 <script>
-import { reactive } from 'vue';
+import { reactive, ref } from 'vue';
 export default {
   name: 'Proxy',
   setup() {
+    // 原始对象
     const originalObj = {
       num: 0
     };
-
+    // 代理对象
     const proxyObj = new Proxy(originalObj, {
       get(target, propKey, receiver) {
         return Reflect.get(target, propKey, receiver);
       },
       set(target, propKey, value, receiver) {
-        console.log(target);
-        console.log(propKey);
-        console.log(value);
-        console.log(receiver);
+        // console.log(target);
+        // console.log(propKey);
+        // console.log(value);
+        // console.log(receiver);
         return Reflect.set(target, propKey, value, receiver);
       }
     });
+    /**
+     * @description 修改原始对象
+     * 修改原始对象，原始对象和代理对象都会改变
+     * */
     const originalHandle = () => {
       originalObj.num += 3;
       console.log(proxyObj);
       console.log(originalObj);
     };
+    /**
+     * @description 修改代理对象
+     * 修改代理对象，原始对象和代理对象都会改变
+     *
+     * 触发代理对象set
+     *
+     */
     const proxyHandle = () => {
       proxyObj.num += 1;
       console.log(proxyObj);
