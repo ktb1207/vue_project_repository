482a37d8a9dd056d4d78a9014281f4f170e5b50b
kongtb <13103820893@163.com>
2022-01-06 22:09:52 +0800

    add low code demo

---
+++ auto-create-codeVue3/src/autoCreatePage/Day.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <KRow align="left" verticalAlign="stretch"
+      ><KCol width="1" flexDirection="row" mainAlign="start" crossAlign="stretch"
+        ><KP fontAlign="right" fontColor="#2cb55c" :fontSize="28" fontWeight="bold" children="保存"
+          >今天是圣诞袜了JFK拉士大夫看见撒旦发安康市分厘卡是否会拉克斯基ask就返回拉萨开发和</KP
+        ></KCol
+      ></KRow
+    ><KRow align="left" verticalAlign="stretch"
+      ><KCol width="1" flexDirection="row" mainAlign="start" crossAlign="stretch"
+        ><KImage imageName="a1.jpeg" imageWidth="auto" imageHeight="auto"></KImage></KCol></KRow
+    ><KRow align="left" verticalAlign="stretch"
+      ><KCol width="160px" flexDirection="row" mainAlign="start" crossAlign="stretch"></KCol
+      ><KCol width="160px" flexDirection="row" mainAlign="start" crossAlign="stretch"></KCol
+      ><KCol width="160px" flexDirection="row" mainAlign="start" crossAlign="stretch"></KCol
+      ><KCol width="160px" flexDirection="row" mainAlign="start" crossAlign="stretch"></KCol></KRow
+    ><KRow align="left" verticalAlign="stretch"
+      ><KCol width="1" flexDirection="row" mainAlign="start" crossAlign="stretch"></KCol
+      ><KCol width="1" flexDirection="row" mainAlign="start" crossAlign="stretch"></KCol></KRow
+    ><KRow align="left" verticalAlign="stretch"
+      ><KCol width="1" flexDirection="row" mainAlign="start" crossAlign="stretch"></KCol
+      ><KCol width="1" flexDirection="row" mainAlign="start" crossAlign="stretch"></KCol
+    ></KRow>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'Day'
+});
+</script>
